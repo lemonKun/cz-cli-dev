1614eac35dd16444ba997ee8740b6cd799cbf7c7
lemonKun <zcai1@leqee.com>
2021-08-03 20:00:53 +0800

    feat: 依赖本地化改进

---
+++ packages/core/bin/core.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+const util = require('@lemon-kun/util')
+util()
+console.log('lemon-kun-cli running!')
--- packages/util/lib/index.js
+++ packages/util/lib/index.js
@@ -1,7 +1,4 @@
-'use strict';
-
-module.exports = util;
-
 function util() {
-    // TODO
+  console.log('import util package success!')
 }
+module.exports = util;
